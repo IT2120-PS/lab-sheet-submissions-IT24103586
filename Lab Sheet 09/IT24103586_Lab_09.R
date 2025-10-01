885a73cb6dc8833676a32fac12e9a67e1225890f
IT24103586 <it24103586@my.sliit.lk>
2025-10-01 22:11:35 +0530

    LAB_09

---
+++ Lab Sheet 09/IT24103586_Lab_09.R
@@ -0,0 +1,20 @@
+setwd("C:\\Users\\thisu\\OneDrive\\Desktop\\IT24103586")
+getwd()
+
+##Exercise
+
+#i
+y <- rnorm(25, mean = 45, sd = 2)
+print(y)
+
+#ii
+t.test(y, mu = 46, alternative = "less")
+
+
+
+
+
+
+
+
+
